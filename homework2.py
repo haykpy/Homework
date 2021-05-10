c1f89f8c02b35ba0d94d8ce20fa493082e5f5964
haykpy <83661749+haykpy@users.noreply.github.com>
2021-05-11 01:52:21 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,74 @@
+class Shape:
+    def __init__(self, attrs):
+    	self.attrs = name
+
+
+    def present_shape():
+    	pass
+
+class Triangle(Shape):
+	def __init__(self, side1, side2, base, height):
+		self.side1 = side1
+		self.side2 = side2
+		self.base = width
+		self.height = length
+
+
+	def present(self):
+		pass
+
+	def area(self):
+		area_1 = width * length
+		return area_1
+
+	def perimeter(self):
+		perimeter_1 = (width + length) * 2
+		return perimeter_1
+
+	
+class Square(Shape):
+	def __init__(self, side1, side2):
+		self.side1 = side1
+		self.side2 = side2
+
+
+	def present(self):
+		pass
+
+
+	def area(self):
+		area_1 = side1 * side2
+		return area_1
+
+	def perimeter(self):
+		perimeter_1 = (side1 + side2) * 2
+		return perimeter_1
+
+	def get_bias(self):
+		pass
+
+		
+# --------------------------------------------------------------------------------------------------
+
+class Country:
+	def __init__(self, name,continent):
+		self.country_name = name
+		self.continent = continent
+		return self.name
+
+class Brand:
+	def brand_name(self):
+		self.name = name_1
+
+	def business_start_date(self):
+		self.date = date
+
+	def presentation(self):
+		self.presentation = pres
+
+
+class Season:
+	pass
+
+
+
